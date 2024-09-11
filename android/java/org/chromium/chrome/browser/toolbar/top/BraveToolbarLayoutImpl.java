8ecfc5a53cbd26a126ae3ab43be1295b00523beb
deeppandya <deeppandya91@gmail.com>
2024-09-11 23:52:47 +0530

    Address PR reviews
    Resolve rebase issues

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
@@ -694,7 +694,6 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
                             playlistItems.add(playlistItem);
                         }
                     }
-                    // if (playlistItems.size() > 0) {
                     mPlaylistService.addMediaFiles(
                             playlistItems.toArray(new PlaylistItem[0]),
                             ConstantUtils.DEFAULT_PLAYLIST,
@@ -704,9 +703,6 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
                                     showAddedToPlaylistSnackBar();
                                 }
                             });
-                    // } else {
-                    //     showAlreadyAddedToPlaylistSnackBar();
-                    // }
                 });
     }
 
