57f1292378fa73e32e39b2e17129b9942c51b644
Claudio DeSouza <cdesouza@brave.com>
2024-09-13 14:31:02 +0100

    [WIP] Migrate Download Manager item polymer overrides.
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/8c8b25bb5e964207b68cc982d718f161ef601f29
    
    commit 8c8b25bb5e964207b68cc982d718f161ef601f29
    Author: rbpotter <rbpotter@chromium.org>
    Date:   Wed Sep 11 17:28:44 2024 +0000
    
        Downloads: Cleanup Polymer leftovers
    
        - Check in .html.ts file for downloads item
        - Migrate icons to cr-iconset
        - Remove Polymer ts_dep, except in branded builds where it is still
          needed for the branded iconset.
    
        Bug: 40943652

--- browser/resources/downloads/brave_overrides/item.ts
+++ browser/resources/downloads/brave_overrides/item.ts
@@ -3,16 +3,16 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import {RegisterStyleOverride} from 'chrome://resources/brave/polymer_overriding.js'
-import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
+// import {RegisterStyleOverride} from 'chrome://resources/brave/polymer_overriding.js'
+// import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js'
 
-RegisterStyleOverride(
-  'downloads-item',
-  html`
-    <style>
-      #progress {
-        --paper-progress-active-color: var(--leo-color-icon-interactive) !important;
-      }
-    </style>
-  `
-)
+// RegisterStyleOverride(
+//   'downloads-item',
+//   html`
+//     <style>
+//       #progress {
+//         --paper-progress-active-color: var(--leo-color-icon-interactive) !important;
+//       }
+//     </style>
+//   `
+// )
